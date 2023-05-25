a781ca40dcd2e7fcac1827f702f6724af1ffe5fc
Timo <timohlu98@gmail.com>
2023-05-25 19:33:19 +0200

    Pilot 2

---
+++ title-cursor.js
@@ -0,0 +1,18 @@
+ function moveText(event) {
+      var div = event.currentTarget;
+      var text = div.querySelector('.text');
+
+      var offsetX = event.clientX;
+      var offsetY = event.clientY;
+
+      text.style.left = offsetX + 'px';
+      text.style.top = offsetY + 'px';
+      text.style.transform = 'translate(-50%, -50%)'; // Center the text
+
+      text.style.display = 'block';
+    }
+
+    function hideText(event) {
+      var text = event.currentTarget.querySelector('.text');
+      text.style.display = 'none';
+    }
